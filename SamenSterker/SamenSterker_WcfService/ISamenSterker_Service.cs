66e2f5c344ed773b251ec7ed6caaef9c445ed432
Sam Neven <Sam.Neven@student.pxl.be>
2014-05-03 11:57:52 +0200

    Scrum + ToDo toegevoegd + connectie klasse DB
    Scrum en ToDo bestanden toegevoegd + connectie klasse voor connectie met
    de database gemaakt

--- SamenSterker/SamenSterker_WcfService/ISamenSterker_Service.cs
+++ SamenSterker/SamenSterker_WcfService/ISamenSterker_Service.cs
@@ -10,7 +10,7 @@ namespace SamenSterker_WcfService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface ISamenSterker_Service
     {
 
         [OperationContract]
---
+++ SamenSterker/SamenSterker_WcfService/SamenSterkerDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SamenSterker_WcfService
+{
+    /// <summary>
+    /// klasse SamenSterkerDB
+    /// hierin wordt de methode GetConnection() gemaakt
+    /// </summary>
+    public static class SamenSterkerDB
+    {
+        /// <summary>
+        /// GetConnection
+        /// zorgt voor de connectie met de database
+        /// </summary>
+        /// <returns> ConnectionString </returns>
+        public static string GetConnection()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["SamenSterker_Conn"].ConnectionString;
+        }
+    }
+}
